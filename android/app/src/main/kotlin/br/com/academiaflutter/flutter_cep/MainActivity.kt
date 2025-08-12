0f28649eac6d0b36ddb7cbe7c1a9037cfdaafb48
andermsilva <67654030+andermsilva@users.noreply.github.com>
2025-08-12 11:43:03 -0300

    create and initial  configuretion

---
+++ android/app/src/main/kotlin/br/com/academiaflutter/flutter_cep/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.com.academiaflutter.flutter_cep
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FlutterCepApp());
+}
+
+class FlutterCepApp extends StatelessWidget {
+  const FlutterCepApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Consulta de CEP',
+      home: Container(),
+    );
+  }
+}
